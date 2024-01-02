eafd319e5e2d68dfc7afca5ce800a945cb8c3ebd
Juan Pablo <juanbermudeznunez@gmail.com>
2024-01-02 10:13:05 -0300

    Add README file to iterations folder

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -19,14 +19,3 @@ func ExampleAdd() {
 	fmt.Println(sum)
 	// Output: 6
 }
-
-// You will notice that we're using %d as our format strings rather than %q. That's because we want it to print an integer rather than a string.
-// Also note that we are no longer using the main package, instead we've defined a package named integers, as the name suggests this will group
-// functions for working with integers such as Add.
-
-// Go examples are executed just like tests so you can be confident examples reflect what the code actually does.
-// Examples are compiled (and optionally executed) as part of a package's test suite.
-// As with typical tests, examples are functions that reside in a package's _test.go files. Add the following ExampleAdd function to the adder_test.go file.
-
-// Please note that the example function will not be executed if you remove the comment // Output: 6. Although the function will be compiled, it won't be executed.
-// By adding this code the example will appear in the documentation inside godoc, making your code even more accessible.
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -16,13 +16,3 @@ func BenchmarkRepeat(b *testing.B) {
 		Repeat("a")
 	}
 }
-
-// Benchmarking
-// Writing benchmarks in Go is another first-class feature of the language and it is very similar to writing tests.
-
-// The testing.B gives you access to the cryptically named b.N.
-// When the benchmark code is executed, it runs b.N times and measures how long it takes.
-// The amount of times the code is run shouldn't matter to you, the framework will determine what is a "good" value for that to let you have some decent results.
-// To run the benchmarks do go test -bench=. (or if you're in Windows Powershell go test -bench=".")
-
-// What 136 ns/op means is our function takes on average 136 nanoseconds to run (on my computer). Which is pretty ok! To test this it ran it 10000000 times.
